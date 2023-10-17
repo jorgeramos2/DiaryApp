a411b591ecee9ccec58ed08d3b4c1ac660dc5348
jorgeramosdx <jxr0722@dexcom.com>
2023-10-17 02:22:08 -0600

    initial commit

---
+++ DiaryAPP/DiaryAPP/DiaryAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryAPPApp.swift
+//  DiaryAPP
+//
+//  Created by Jorge Ramos on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct DiaryAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
